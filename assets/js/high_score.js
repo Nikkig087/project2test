fa9276030ddd99f952cdd9dee90ca6f451c2f5d9
NikkiGetsee <123640851+Nikkig087@users.noreply.github.com>
2024-04-09 20:48:21 +0000

    fixed issue on laptop screen with wrong answer

---
+++ assets/js/high_score.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("backButton").addEventListener("click", goToIndex);
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    displayHighScores();
+});
+
+
+function displayHighScores() {
+    const highScoresList = document.getElementById("highScoresList");
+    const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+  
+    highScoresList.innerHTML ="";
+    
+    const heading = document.createElement("h1");
+    heading.textContent = "High Scores";
+    heading.classList.add("text-center", "mb-4");
+
+    highScoresList.appendChild(heading);
+    
+    const table = document.createElement("table");
+    table.classList.add("table");
+
+  
+    const headerRow = document.createElement("tr");
+    const usernameHeader = document.createElement("th");
+    usernameHeader.textContent = "Username";
+    headerRow.appendChild(usernameHeader);
+    const scoreHeader = document.createElement("th");
+    scoreHeader.textContent = "Score";
+    headerRow.appendChild(scoreHeader);
+    table.appendChild(headerRow);
+
+
+    highScores.forEach(score => {
+        const row = document.createElement("tr");
+        const usernameCell = document.createElement("td");
+        usernameCell.textContent = score.username;
+        row.appendChild(usernameCell);
+        const scoreCell = document.createElement("td");
+        scoreCell.textContent = score.score;
+        row.appendChild(scoreCell);
+        table.appendChild(row);
+    });
+
+    
+    highScoresList.appendChild(table);
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,6 @@ let currentCharacter;
 let previousCharacter;
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("backButton").addEventListener("click", goToIndex);
-});
-
 
 
 
@@ -136,61 +132,6 @@ function checkGuess() {
 
  
     document.getElementById("correctAnswers").textContent = `Correct answers: ${correctAnswers}`;
-}
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    displayHighScores();
-});
-
-
-function displayHighScores() {
-   
-
-    const highScoresList = document.getElementById("highScoresList");
-    if (!highScoresList) {
-        console.error("highScoresList element not found.");
-        return;
-    }
-
-    const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-    // Clear the existing content of the list
-    highScoresList.innerHTML = "";
-   
-    
-    const heading = document.createElement("h1");
-    heading.textContent = "High Scores";
-    heading.classList.add("text-center", "mb-4");
-
-    highScoresList.appendChild(heading);
-    
-    const table = document.createElement("table");
-    table.classList.add("table");
-
-  
-    const headerRow = document.createElement("tr");
-    const usernameHeader = document.createElement("th");
-    usernameHeader.textContent = "Username";
-    headerRow.appendChild(usernameHeader);
-    const scoreHeader = document.createElement("th");
-    scoreHeader.textContent = "Score";
-    headerRow.appendChild(scoreHeader);
-    table.appendChild(headerRow);
-
-
-    highScores.forEach(score => {
-        const row = document.createElement("tr");
-        const usernameCell = document.createElement("td");
-        usernameCell.textContent = score.username;
-        row.appendChild(usernameCell);
-        const scoreCell = document.createElement("td");
-        scoreCell.textContent = score.score;
-        row.appendChild(scoreCell);
-        table.appendChild(row);
-    });
-
-    
-    highScoresList.appendChild(table);
 }
 
 function gameOver() {
